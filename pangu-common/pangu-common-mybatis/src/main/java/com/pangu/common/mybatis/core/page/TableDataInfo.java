5bb2d08b7850a89f24cbe641ab48c825fbe3043f
chengliang4810 <chengliang4810@163.com>
2023-01-05 17:43:50 +0800

    refactor(common):  TableDataInfo.java 将分页数据存放在data中

--- pangu-common/pangu-common-mybatis/src/main/java/com/pangu/common/mybatis/core/page/TableDataInfo.java
+++ pangu-common/pangu-common-mybatis/src/main/java/com/pangu/common/mybatis/core/page/TableDataInfo.java
@@ -6,7 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 表格分页数据对象
@@ -20,16 +22,6 @@ public class TableDataInfo<T> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 总记录数
-     */
-    private long total;
-
-    /**
-     * 列表数据
-     */
-    private List<T> rows;
-
-    /**
      * 消息状态码
      */
     private int code;
@@ -40,22 +32,29 @@ public class TableDataInfo<T> implements Serializable {
     private String msg;
 
     /**
+     * 数据
+     * rows = 分页数据
+     * total = 总记录数
+     */
+    private Map<String, Object> data = new HashMap<>(3);
+
+    /**
      * 分页
      *
      * @param list  列表数据
      * @param total 总记录数
      */
     public TableDataInfo(List<T> list, long total) {
-        this.rows = list;
-        this.total = total;
+        this.data.put("rows", list);
+        this.data.put("total", total);
     }
 
     public static <T> TableDataInfo<T> build(IPage<T> page) {
         TableDataInfo<T> rspData = new TableDataInfo<>();
         rspData.setCode(HttpStatus.HTTP_OK);
         rspData.setMsg("查询成功");
-        rspData.setRows(page.getRecords());
-        rspData.setTotal(page.getTotal());
+        rspData.data.put("rows", page.getRecords());
+        rspData.data.put("total", page.getTotal());
         return rspData;
     }
 
@@ -63,8 +62,8 @@ public class TableDataInfo<T> implements Serializable {
         TableDataInfo<T> rspData = new TableDataInfo<>();
         rspData.setCode(HttpStatus.HTTP_OK);
         rspData.setMsg("查询成功");
-        rspData.setRows(list);
-        rspData.setTotal(list.size());
+        rspData.data.put("rows", list);
+        rspData.data.put("total", list.size());
         return rspData;
     }
 
@@ -75,4 +74,21 @@ public class TableDataInfo<T> implements Serializable {
         return rspData;
     }
 
+    /**
+     * 设置列表数据
+     *
+     * @param list 列表
+     */
+    public void setRows(List<T> list){
+        this.data.put("rows", list);
+    }
+
+    /**
+     * 设置总条数
+     *
+     * @param size 大小
+     */
+    public void setTotal(int size) {
+        this.data.put("total", size);
+    }
 }
