d54a3d0f55474a891288b13b671b34b17ffb6d2b
chengliang4810 <chengliang4810@163.com>
2023-01-30 17:35:15 +0800

    feat(iot-data): 更新代码

--- pangu-iot/pangu-iot-data/src/main/java/com/pangu/iot/data/config/MybatisPlusConfig.java
+++ pangu-iot/pangu-iot-data/src/main/java/com/pangu/iot/data/config/MybatisPlusConfig.java
@@ -1,17 +1,22 @@
 package com.pangu.iot.data.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.dialects.PostgreDialect;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisPlusConfig {
 
-    @Bean
-    public void paginationInnerInterceptor(@NotNull PaginationInnerInterceptor interceptor){
-        interceptor.setDialect(new PostgreDialect());
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Autowired
+    public void mybatisPlusInterceptor(MybatisPlusInterceptor interceptor) {
+        // 添加自动分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
     }
+
 
 }
