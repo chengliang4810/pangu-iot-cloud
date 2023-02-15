c73e39f62482acf0c85b7cdc0f18647c3246fcde
chengliang4810 <chengliang4810@163.com>
2023-02-15 17:28:48 +0800

    feat(iot-common): zabbix config

--- pangu-common/pangu-common-sdk/src/main/java/com/pangu/common/sdk/camel/CamelConfig.java
+++ pangu-common/pangu-common-sdk/src/main/java/com/pangu/common/sdk/camel/CamelConfig.java
@@ -1,10 +1,29 @@
 package com.pangu.common.sdk.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CamelConfig {
 
+
+    @Bean
+    CamelContextConfiguration contextConfiguration() {
+        return new CamelContextConfiguration() {
+            @Override
+            public void beforeApplicationStart(CamelContext context) {
+                // your custom configuration goes here
+                context.addComponent("zabbix", new ZabbixDefaultComment());
+            }
+
+            @Override
+            public void afterApplicationStart(CamelContext camelContext) {
+
+            }
+        };
+    }
 
 
 
