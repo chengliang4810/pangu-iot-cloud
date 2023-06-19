e94a578a3df7905e75aec15897100a673cbc1df6
chengliang4810 <chengliang4810@163.com>
2023-06-19 09:26:02 +0800

    feat: 新增iot-data模块

---
+++ ruoyi-modules/ruoyi-data/src/main/java/org/dromara/data/RuoYiDataApplication.java
@@ -0,0 +1,22 @@
+package org.dromara.data;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+/**
+ * IOT管理模块
+ *
+ * @author chengliang4810
+ */
+@EnableDubbo
+@SpringBootApplication
+public class RuoYiDataApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(RuoYiDataApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  IOT数据模块启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
