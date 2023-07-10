0f0f58ad3def9b70fcb07c9bcc3cc60a4d911b69
chengliang4810 <chengliang4810@163.com>
2023-07-10 17:43:18 +0800

    feat: 定义设备属性上报TOPIC

---
+++ ruoyi-common/ruoyi-common-iot/src/main/java/org/dromara/common/iot/constant/DeviceAttributeTopic.java
@@ -0,0 +1,43 @@
+package org.dromara.common.iot.constant;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 设备属性相关主题
+ *
+ * @author chengliang4810
+ * @date 2023/07/10
+ */
+public interface DeviceAttributeTopic {
+
+    /**
+     * 设备属性上报主题模板
+     */
+    String DEVICE_ATTRIBUTE_REPORT_TOPIC_TPL = "device/{}/property/report";
+
+    /**
+     * 设备属性上报主题
+     *
+     * @param deviceId 设备唯一标识
+     * @return {@link String}
+     */
+    static String getDeviceAttributeReportTopic(String deviceId) {
+        return StrUtil.format(DEVICE_ATTRIBUTE_REPORT_TOPIC_TPL);
+    }
+
+    /**
+     * 设备属性上报主题
+     *
+     * @param deviceId   设备唯一标识
+     * @return {@link String}
+     */
+    static String getDeviceAttributeReportTopic(Long deviceId) {
+        return StrUtil.format(DEVICE_ATTRIBUTE_REPORT_TOPIC_TPL, deviceId);
+    }
+
+    /**
+     * 设备属性上报订阅主题
+     */
+    String DEVICE_STATUS_SUB_TOPIC = "device/+/property/report";
+
+}
