2d773cf3107dd254d2ae0fa02335da9b57b884ba
chengliang4810 <chengliang4810@163.com>
2023-07-14 16:39:56 +0800

    feat: 新增属性值转换脚本模板

---
+++ ruoyi-modules/ruoyi-manager/src/main/java/org/dromara/manager/data/constant/ScriptConstants.java
@@ -0,0 +1,16 @@
+package org.dromara.manager.data.constant;
+
+/**
+ * 脚本常量
+ *
+ * @author chengliang
+ * @date 2023/07/14
+ */
+public interface ScriptConstants {
+
+    /**
+     * 属性值转换脚本模板
+     */
+    String ATTRIBUTE_VALUE_SCRIPT_TPL = "function convertValue(value){ {} }; convertValue(\"{}\")";
+
+}
