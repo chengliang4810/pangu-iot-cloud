283bbed4d3a39b908007c07c3bc85c588316a6a5
chengliang4810 <chengliang4810@163.com>
2023-07-18 01:15:11 +0800

    feat: 补充存储mqtt主题

---
+++ ruoyi-common/ruoyi-common-iot/src/main/java/org/dromara/common/iot/constant/StorageTopic.java
@@ -0,0 +1,41 @@
+package org.dromara.common.iot.constant;
+
+/**
+ * 存储相关主题
+ *
+ * @author chengliang4810
+ * @date 2023/07/17
+ */
+public interface StorageTopic {
+
+    /**
+     * 存储设备属性
+     */
+    String STORAGE_DEVICE_PROPERTY_TOPIC_TPL = "storage/device/{}/property";
+
+    /**
+     * 获取存储设备属性主题
+     *
+     * @param deviceId 设备唯一标识
+     * @return {@link String}
+     */
+    static String getStorageDevicePropertyTopic(String deviceId) {
+        return STORAGE_DEVICE_PROPERTY_TOPIC_TPL.replace("{}", deviceId);
+    }
+
+    /**
+     * 获取存储设备属性主题
+     *
+     * @param deviceId 设备唯一标识
+     * @return {@link String}
+     */
+    static String getStorageDevicePropertyTopic(Long deviceId) {
+        return STORAGE_DEVICE_PROPERTY_TOPIC_TPL.replace("{}", String.valueOf(deviceId));
+    }
+
+    /**
+     * 存储设备属性订阅主题
+     */
+    String DEVICE_STATUS_SUB_TOPIC = "storage/device/+/property";
+
+}
